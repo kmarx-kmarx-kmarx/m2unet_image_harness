3aac68e26f2bcbde352d6195584f8d12e1ea751d
Kevin Marx <kevinmarx14@gmail.com>
2023-03-23 11:03:29 -0700

    added to refactored npz demo

--- demo_npz.py
+++ demo_npz.py
@@ -1,119 +1,90 @@
-# NOTE: due to refactor in progress, this currently does not work
-import cv2
-import numpy as np
-from run_on_image import inference_on_image_stack
-import os
-import random
 import glob
+import random
+import os
 import time
-from tqdm import tqdm
 from natsort import natsorted
-import torch
-import torch.nn.functional as F
-import pandas as pd
+from m2unet import m2unet
+import cv2
+from run_on_image import inference_on_image_stack
+import numpy as np
 
 def main():
-    data_dir = "/home/prakashlab/Documents/kmarx/train_m2unet_cellpose_cloud/data_uganda_hemaprep"
-    model_root = "/home/prakashlab/Documents/kmarx/train_m2unet_cellpose_cloud/m2unet_model_flat_less_erode_2_rotfirst"
-    model_name = "model_24_9.pth"
-    save_dir = "results_flat_erode_rotfirst_with_eroded_masks_final_ord"
-    n_im = 50 # set to None to run on all available .npz
-    randomize = False
-    erode_mask = 1
-    center_crop_sz = 0
+    data_dir = "path/to/data"
+    model_root = "m2unet_model_greyscale"
+    n_channels = 1 # set to 1 for greyscale, 3 for RGB
+    model_name = "model_70_11.pth"
+    save_dir = "path/to/save"
+    batch_sz = 1 # segment this many images per batch. Should be set depending on image size, hardware.
+    n_im = 1    # total number of images to segment. Set to 0 to segment all images in data_dir
+    run_size = 1024 # tile the images to 1024x1024 sections. Should be set depending on image size, hardware
+    overlap = 16    # Amount of overlap between adjacent tiles
+    randomize = False # Segment the images in random order
+    save_cp_mask = False
+    save_image = False
+    save_diff = True
+    save_pred_mask = False
     random.seed(3)
+
+    # Load M2Unet
+    model, device = m2unet(model_root, model_name, upsamplemode='bilinear',expand_ratio=0.15, output_channels=1, activation="linear", N_CHAN=n_channels)
 
     os.makedirs(save_dir, exist_ok=True)
 
-    # Get image paths
-    images = natsorted(glob.glob(os.path.join(data_dir, "**.npz"), recursive=True))
+    # get image paths
+    images = natsorted(glob.glob(os.path.join(data_dir, f"**_seg.npz")))
     if randomize:
         random.shuffle(images)
-    if n_im != None:
+    if n_im > 0:
         images = images[:n_im]
+    total_images = len(images)
 
-    # assume all images are the same shape - get shape info from first image
-    data = np.load(images[0])
-    im = data["img"]
-    sh = im.shape
-    image_stack = np.zeros((len(images), sh[0], sh[1]))
-    mask_stack = np.zeros((len(images), sh[0], sh[1]))
-    if center_crop_sz > 0:
-        x = sh[1]/2 - center_crop_sz/2
-        y = sh[0]/2 - center_crop_sz/2
-    t0 = time.time()
-    
-    for i, image_path in enumerate(tqdm(images)):
-        data = np.load(image_path)
-        im = data["img"]
-        mask = data["mask"]
-        image_stack[i,:,:] = im
-
-        # save data as images
-        fname = image_path.rsplit('.', 1)[0]
-        fname = fname.split('/')[-1]
-        if erode_mask > 0:
-            shape = cv2.MORPH_ELLIPSE
-            element = cv2.getStructuringElement(shape, (2 * erode_mask + 1, 2 * erode_mask + 1), (erode_mask, erode_mask))
-            mask = np.array(cv2.erode(mask, element))
-        mask_stack[i,:,:] = mask
-
-        if center_crop_sz > 0:
-            im = im[int(y):int(y+center_crop_sz), int(x):int(x+center_crop_sz)]
-            mask = mask[int(y):int(y+center_crop_sz), int(x):int(x+center_crop_sz)]
-
+    # Load and evaluate batch_sz images at a time
+    idx = 0
+    t_read = []
+    t_segm = []
+    t_writ = []
+    t_init = time.time()
+    while idx < total_images:
+        end_idx = min(idx + batch_sz, total_images)
+        # Load the images
+        imgs = []
+        masks = []
+        for i in range(idx, end_idx):
+            t0 = time.time()
+            data = np.load(images[i])
+            im = data["img"]
+            mask = data["mask"]
+            imgs.append(im)
+            masks.append(mask)
+            t_read.append(time.time()-t0)
+        imgs = np.array(imgs)
+        # get results
+        results, times = inference_on_image_stack(imgs, model, device=device, sz=run_size, overlap=overlap)
+        t_segm = t_segm + times
+        # save results
+        for i, result in enumerate(results):
+            t0 = time.time()
+            fname = images[i+idx].split('/')[-1]
+            if save_pred_mask:
+                savepath = os.path.join(save_dir, f"seg_{fname}")
+                cv2.imwrite(savepath, 255*result[:,:,0])
+            if save_cp_mask:
+                savepath = os.path.join(save_dir, f"cp_{fname}")
+                cv2.imwrite(savepath, 255*masks[i])
+            if save_image:
+                savepath = os.path.join(save_dir, f"im_{fname}")
+                cv2.imwrite(savepath, imgs[i])
+            if save_diff:
+                asdfasdfasd
+            t_writ.append(time.time()-t0)
         
-        cv2.imwrite(os.path.join(save_dir, f"{fname}_original.png"), im)
-        # cv2.imwrite(os.path.join(save_dir,(f"{fname}_mask.png")), mask)
-    print(image_stack.shape)
-    t0 = time.time()
-    result, __, times = inference_on_image_stack(image_stack, model_root, model_name)
-    print(np.median(times))
-    print(time.time()-t0)
-    result=result.astype(np.uint8)
-
-    jaccard = []
-    bce = []
-    for i, segmented in enumerate(tqdm(result)):
-        # save data as images
-        jaccard.append(jaccard_similarity_bin_mask(mask_stack[i,:,:], segmented))
-        bce.append(binary_cross_entropy(mask_stack[i,:,:], segmented))
-        if center_crop_sz > 0:
-            segmented = segmented[int(y):int(y+center_crop_sz), int(x):int(x+center_crop_sz)]
-            mask = mask_stack[i, int(y):int(y+center_crop_sz), int(x):int(x+center_crop_sz)]
-            sh = segmented.shape
-        segmented = segmented * 255
-        fname = images[i].rsplit('.', 1)[0]
-        fname = fname.split('/')[-1]
-        diff = (segmented/np.max(segmented)) - (mask/np.max(mask))
-        color_diff = np.zeros((sh[0], sh[1], 3), dtype=np.uint8)
-        color_diff[:,:,0] = (255 * (diff < 0)).astype(np.uint8)
-        color_diff[:,:,2] = (255 * (diff > 0)).astype(np.uint8)
-        # cv2.imwrite(os.path.join(save_dir, f"{fname}_diff.png"), color_diff)
-        cv2.imwrite(os.path.join(save_dir, f"{fname}_pred.png"), segmented)
-    #make csv
-    data_out = {"paths": images, "jaccard": jaccard, "bce": bce}
-    df = pd.DataFrame(data_out)
-    df.to_csv(f'{save_dir}/hema_uganda_2.csv')
-
-def jaccard_similarity_bin_mask(A, B):
-    # Calculate Jaccard similarity of two masks
-    intersection = np.sum(np.logical_and(A, B))
-    union = np.sum(np.logical_or(A, B))
-    res = intersection/union
-    return res
-
-def binary_cross_entropy(y_true, y_pred):
-    # Convert to tensors
-    y_true = y_true/np.max(y_true)
-    y_pred = y_pred/np.max(y_pred)
-    y_true = torch.tensor(y_true).float()
-    y_pred = torch.tensor(y_pred).float()
-
-    # Calculate binary cross-entropy
-    bce_loss = F.binary_cross_entropy(y_pred, y_true)
-
-    return bce_loss.item()
-
+        idx = end_idx
+    
+    # print timing statistics
+    print(f"Reading:      {np.sum(t_read):.3f}, avg {np.mean(t_read):.3f}")
+    print(f"Segmentation: {np.sum(t_segm):.3f}, avg {np.mean(t_segm):.3f}")
+    print(f"Writing:      {np.sum(t_writ):.3f}, avg {np.mean(t_writ):.3f}")
+    print(f"Total:        {(time.time()-t_init):.3f}, avg {(time.time()-t_init)/total_images:.3f}")
+    
 if __name__ == "__main__":
     main()
